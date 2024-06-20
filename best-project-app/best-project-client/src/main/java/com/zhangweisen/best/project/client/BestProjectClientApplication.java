17d7b6c3db5b1d2c6967b80df066c963f3176d0d
张威森 <zws812000@sina.cn.com>
2024-06-20 17:22:56 +0800

    fix: update

--- best-project-app/best-project-client/src/main/java/com/zhangweisen/best/project/client/BestProjectClientApplication.java
+++ best-project-app/best-project-client/src/main/java/com/zhangweisen/best/project/client/BestProjectClientApplication.java
@@ -2,8 +2,10 @@ package com.zhangweisen.best.project.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.zhangweisen.best.project.client", "com.zhangweisen.best.project.integration"}) // 确保包含TongYiService的包路径
 public class BestProjectClientApplication {
 
     public static void main(String[] args) {
---
+++ best-project-integration/src/test/java/com/zhangweisen/best/project/integration/BestProjectIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangweisen.best.project.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestProjectIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
